0869097699a903d34c497c7f55a05840586fd254
rahul7840 <coading2020@gmail.com>
2024-02-06 14:56:29 +0530

    oo

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,11 +4,14 @@ import { SignUpDto } from "./Dto/signUp.dto";
 @Injectable()
 export class AuthService {
  
-
+// constructor(private prismas)
 
 
     async signUp(dto:SignUpDto){
         const {fullName,userName,email,password,address,phoneNumber,gender,profilePicture} = dto
+        
+        let check = await this.
+
 
     }
 }
