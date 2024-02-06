fc196003e7483ea8cb2a7e63f43c1eed1f0a3620
rahul7840 <coading2020@gmail.com>
2024-02-06 19:35:43 +0530

    oo

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, ParseIntPipe, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { SignUpDto } from "./Dto/signUp.dto";
 import {ApiOperation,ApiResponse,ApiTags,} from "@nestjs/swagger";
+import { LogInDto } from "./Dto/logIn.dto";
 
 
 @ApiTags('Auth')
@@ -20,5 +21,11 @@ export class AuthController {
     @Post('signup')
     SignUp(@Body()dto:SignUpDto, ParseIntPipe) {
       return this.authService.signUp(dto);
+    }
+
+
+    @Post('/login')
+    async login(@Body() dto: LogInDto) {
+      return await this.authService.logIn(dto);
     }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,8 @@
 import { BadRequestException, Injectable } from "@nestjs/common";
 import { SignUpDto } from "./Dto/signUp.dto";
 import { PrismaService } from "prisma/prisma.service";
+import * as bcrypt from 'bcrypt';
+import { LogInDto } from "./Dto/logIn.dto";
 
 @Injectable()
 export class AuthService{
@@ -28,8 +30,7 @@ async signUp(dto: SignUpDto) {
             return { message: 'Username already exists. Please choose a different username',status:'401'}
         }
 
-        // Optionally hash the password
-        // const hashedPassword = await hashPassword(dto.password);
+        const hashedPassword = await bcrypt.hash(dto.password,10);    
 
         let createUser = await this.prisma.user.create({
             data: {
@@ -37,23 +38,35 @@ async signUp(dto: SignUpDto) {
                 user_name: dto.userName,
                 phone_number: dto.phoneNumber,
                 email: dto.email,
-                password: dto.password,  
+                password: hashedPassword,  
                 address: dto.address,
                 gender: dto.gender,
             }
         });
 
-        // Optionally return user information or JWT token
         return {
             message: 'User created successfully',
             user: createUser,
             // token: generateToken(createUser),
         };
     } catch (error) {
-        // Handle the error and return a meaningful response
-        console.error('Error creating user:', error);
         throw new BadRequestException('Error creating user');
     }
 }
+
+async logIn(dto:LogInDto){
+   const user = await this.prisma.user.findFirst({
+    where:{
+         email: dto.email 
+    }
+   })
+   if (!user) {
+    return { message: 'Invalid credentials', status: 401 };
+}
+  const passwordMatch = await bcrypt.compare(dto.password, user.password);
+  if (passwordMatch) return { message: 'Login successful', status: 200 };
+ else return { message: 'Invalid credentials', status: 401 };
+}
+
 
 }
